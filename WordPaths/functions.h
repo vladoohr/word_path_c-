3708aa088d27140b571071f4c105d5ea2c3ad301
Vladimir Tomanovski <vladimir.tomanovski@seavus.com>
2015-07-20 10:08:05 +0200

    Initial version

---
+++ WordPaths/functions.h
@@ -0,0 +1,13 @@
+#ifndef _FUNCTION_H
+#define _FUNCTION_H
+
+#include "node.h"
+#include <string>
+#include <vector>
+
+std::vector<std::string> all_words();
+int diff_char(std::string start_word, std::string end_word);
+std::vector<std::string> generate_words(Node *node, std::vector<std::string> dictionary);
+std::vector<std::string> path(Node *node);
+
+#endif
